d3de737006a124bf799ccabcdf695bfc06ff6cc8
mmm <mmm.freda@gmail.com>
2025-10-20 18:00:13 +0800

    Update Review.js

--- Review.js
+++ Review.js
@@ -2,13 +2,13 @@ const reviewContainer = document.getElementById("review-container");
 
 // 評價資料
 const reviews = [
-  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B02.JPG?raw=true", title: "鯊克鞋", review: "在溪邊拍攝也不怕，鞋子完全沒滲水，超實用！", rating: 5, link: "https://www.gtselect.com/products/shark-shoes2" },
+  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B02.JPG?raw=true", title: "Shark防水鞋", review: "在溪邊拍攝也不怕，鞋子完全沒滲水，超實用！", rating: 5, link: "https://www.gtselect.com/products/shark-shoes2" },
   { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B04.PNG?raw=true", title: "雲朵水波鞋", review: "鞋底很有彈性，很輕盈，到外縣市輕旅行必穿。", rating: 5, link: "https://www.gtselect.com/products/cloudshoes" },
-  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B01.jpg?raw=true", title: "鯊克鞋", review: "走一整天都不會累，真的比我之前穿過的鞋子都舒服。", rating: 5 , link: "https://www.gtselect.com/products/shark-shoes2" },
+  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B01.jpg?raw=true", title: "Shark防水鞋", review: "走一整天都不會累，真的比我之前穿過的鞋子都舒服。", rating: 5 , link: "https://www.gtselect.com/products/shark-shoes2" },
   { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B05.PNG?raw=true", title: "凌豹鞋", review: "實際穿了兩週，穿著去海邊踏浪也不擔心，Y2K銀色好好看。", rating: 5 , link: "https://www.gtselect.com/products/lepoard" },
-  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B06.jpg?raw=true", title: "鯊克鞋", review: "第一次買這個牌子，原本擔心白色不耐髒，沒想到用清水就能洗乾淨", rating: 5 , link: "https://www.gtselect.com/products/shark-shoes2" },
-  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B07.jpg?raw=true", title: "鯊克鞋", review: "設計簡單大方，不管搭牛仔褲還是運動褲都很好看。", rating: 5 , link: "https://www.gtselect.com/products/shark-shoes" },
-  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B03.JPG?raw=true", title: "鯊克鞋", review: "防水功能很棒，同時又能透氣，值得回購！", rating: 5 , link: "https://www.gtselect.com/products/shark-shoes2" }
+  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B06.jpg?raw=true", title: "Shark防水鞋", review: "第一次買這個牌子，原本擔心白色不耐髒，沒想到用清水就能洗乾淨", rating: 5 , link: "https://www.gtselect.com/products/shark-shoes2" },
+  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B07.jpg?raw=true", title: "Shark防水鞋", review: "設計簡單大方，不管搭牛仔褲還是運動褲都很好看。", rating: 5 , link: "https://www.gtselect.com/products/shark-shoes" },
+  { img: "https://github.com/vobo-is-vobo/vobo-homepage/blob/main/B03.JPG?raw=true", title: "Shark防水鞋", review: "防水功能很棒，同時又能透氣，值得回購！", rating: 5 , link: "https://www.gtselect.com/products/shark-shoes2" }
 ];
 
 // 這裡新增一個評價資料循環
@@ -72,3 +72,4 @@ startScroll();
 // 滑鼠懸停暫停
 reviewContainer.addEventListener("mouseenter", stopScroll);
 reviewContainer.addEventListener("mouseleave", startScroll);
+
